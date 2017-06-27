9d15a77162295ba71f2d56e3c0006cb5f96fa8d2
gkatilevicius <gediminas.katilevicius@nfq.lt>
2017-06-27 22:25:38 +0300

    shake selected character

--- frontend/src/app/scenes/characterSelect/characterSelect.component.ts
+++ frontend/src/app/scenes/characterSelect/characterSelect.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Store} from "@ngrx/store";
 import * as fromRoot from './../../common/index';
 import * as actions from './../../common/actions/character.actions';
@@ -11,20 +11,26 @@ import {State} from "../../common/reducers/character.reducer";
     templateUrl: './characterSelect.component.html',
     styleUrls: ['./characterSelect.component.css']
 })
-export class CharacterSelectComponent {
+export class CharacterSelectComponent implements OnInit{
     types = [];
     pageValid = null;
+    selected = null;
 
     constructor(private store: Store<fromRoot.AppState>, private router:Router) {
         this.types = CharacterTypes;
+    }
+
+    ngOnInit(): void {
         this.store.select('character').subscribe((state: State) => {
             if (state.character.type) {
                 this.pageValid = true;
+                this.selected = state.character.type;
             }
         });
     }
 
     selectCharacter(type: string): void {
+        this.selected = type;
         this.store.dispatch(new actions.SelectCharacterColor(type));
     }
 
